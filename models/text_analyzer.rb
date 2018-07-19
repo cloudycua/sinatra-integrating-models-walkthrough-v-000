a7369a1d16233eb274c88e339cef36968aba9d7f
Christine Cua <cloudycua@gmail.com>
2018-07-19 17:23:19 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -25,7 +25,14 @@ class TextAnalyzer
     arr2 = {}
 
     arr1.map do |c|
-      arr2[2] = arr.count(c)
+      arr2[c] = arr.count(c)
+    end
+
+    def most_used_letter
+      no_spaces = text.gsub(" ", "")
+      letters = no_spaces.split("")
+      histogram = Hash[*letters.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+      histogram.max_by{ |k, v| v }
     end
 
     biggest = { arr1.keys.first => arr2.values.first }
